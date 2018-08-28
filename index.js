519b597cf9de2583e26dd3869be5c4b21276275b
Gabriel <yemolai@gmail.com>
2018-08-28 15:02:04 -0300

    correção de chamada de método

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('./config.json');
 const { createReaders } = require('./pointLineReader');
 
 debug.log(true, true, 'daemon iniciado.');
-config.debug ? log('config:', config) : null;
+config.debug ? debug.log('config:', config) : null;
 
 let readers = [];
 let timer = null;
@@ -22,7 +22,7 @@ const offline = {
 
 const ciclo = (offline) => {
   if (readers !== null) readers.map(r => r.kill());
-  config.debug ? log('reading...') : null;
+  config.debug ? debug.log('reading...') : null;
   readers = createReaders(offline);
   clearTimeout(timer);
   timer = setTimeout(ciclo, intervalo, offline);
